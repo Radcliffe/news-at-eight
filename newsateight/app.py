63bcae0c552a8127d6b5209696e33b8c5a9302ab
David Radcliffe <dradcliffe@gmail.com>
2018-06-10 23:00:50 -0500

    Second commit

---
+++ newsateight/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+from fastText import load_model
+
+app = Flask(__name__, static_url_path='/static')
+model = load_model('ag-model.ftz')
+
+@app.route('/topic', methods=['POST'])
+def predict_topic():
+    text = request.form['text'].replace('\n', ' ')
+    category, probability = model.predict(text)
+    return jsonify({
+        'category': category[0].replace('__label__', ''),
+        'prob': probability[0]
+    })
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=7000)
