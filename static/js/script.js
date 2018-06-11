689c5448ef1d21940bb3f95dfe904b48a3d64ae7
David Radcliffe <dradcliffe@gmail.com>
2018-06-10 22:49:59 -0500

    Here goes

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+$( document ).ready(function () {
+
+  var inputElement = $('input');
+  var textArea = $('textarea');
+  var answerElement = $('#answer');
+  var textbox = $('.textbox');
+  var eball = $('.eball');
+
+  eball.mouseenter ( function(){
+    var text = textArea.val().trim();
+    if (text) {
+      $.post( "topic", { text: text })
+      .done(function( data ) {
+        textbox.html(data.category)
+      });
+    }
+  });
+});
